b1dc426f379782602435be42f6391bcd9bae1f7f
cfuses <cfuses@ub.edu>
2025-04-03 14:05:17 +0200

    parsing of sed.h5 outputs into txt table

--- results_visualitzation/output_proc.py
+++ results_visualitzation/output_proc.py
@@ -1,30 +1,45 @@
 #!/usr/bin/env python3
 
-#%%## score_variants output inspection ###
+#%%## score_variants output rearrangement ###
 # run with enrollhd conda environment
 
 import h5py
 import pandas as pd
 
+# Path to save results
+outdir = "/pool01/projects/abante_lab/genomic_llms/borzoi/test_outputs/"
+
+# Path of borzoi output
 output_path = "/pool01/code/src/genomic_llms/borzoi/tutorials/latest/score_variants/snp_sed/f0c0/sed.h5"
 
 #%%## Open the .h5 file ###
-f = h5py.File(output_path,'r')
-# Inspect the contents of the .h5 file
-print("Keys in the HDF5 file:", list(f.keys()))
+sed_h5 = h5py.File(output_path,'r')
 
-# Optionally, inspect the structure of each key
-for key in f.keys():
-    print(f"Contents of key '{key}':")
-    print(f[key])
+# Extract variables
+snp_list = [s.decode() for s in sed_h5['snp']]
+gene_list = [g.decode() for g in sed_h5['gene']]
+snp_idx = sed_h5['si'][:]
+logseds = sed_h5['logSED'][:]
+target_labels = [t.decode() for t in sed_h5['target_labels']]
+alt_alleles = [a.decode() for a in sed_h5['alt_allele']]
 
-#%% Extract contribution (from borzoi run_variant_scripts.ipynb)
-# Print an example variant effect prediction for a SNP-gene pair (gene-specific expression)
+results = []
 
-sed_h5 = h5py.File(output_path, 'r')
+for row_ix, snp_gene_idx in enumerate(snp_idx):
+    snp = snp_list[snp_gene_idx]
+    gene = gene_list[row_ix]
+    alt_allele = alt_alleles[snp_gene_idx]  
+    for target_ix, target_label in enumerate(target_labels):
+        results.append({
+            "snp": snp,
+            "gene": gene,
+            "alt_allele": alt_allele,
+            "tissue": target_label,
+            "logSED": logseds[row_ix, target_ix]
+        })
 
-row_ix = 23
-target_ix = 0
+# Convert results to a pandas DataFrame
+df = pd.DataFrame(results)
 
-print("score: 'logSED', snp: '" + str(sed_h5['snp'][sed_h5['si'][row_ix]].decode()) + "', gene: '" + str(sed_h5['gene'][sed_h5['si'][row_ix]].decode()) + "', track: '" + str(sed_h5['target_labels'][target_ix].decode()) + "' => " + str(round(sed_h5['logSED'][row_ix, target_ix], 4)))
-# %%
+# Save the DataFrame to a tab separated file
+df.to_csv(f"{outdir}sed.txt", sep="\t", index=False)
